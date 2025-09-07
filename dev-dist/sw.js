84cae94fc300d41019cf9691e86556aa32f4dbe0
Amine Oudjana <amine.oudjana@withsecure.com>
2025-09-07 20:45:08 +0200

    Update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.plh0q486un"
+    "revision": "0.25dh6m7c818"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
